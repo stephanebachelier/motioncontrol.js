a3589573859c5ba22a2c4a497aaf542a3c486b07
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-11-13 23:43:39 +0100

    chore(umd): export motioncontrol

--- tasks/umd.js
+++ tasks/umd.js
@@ -1,6 +1,7 @@
 module.exports = {
   lib: {
     indent: '  ',
+    objectToExport: 'motioncontrol',
     src: 'dist/<%= pkg.name.replace(/\.js$/, "") %>.js',
     dest: 'dist/<%= pkg.name.replace(/\.js$/, "") %>.js'
   }
