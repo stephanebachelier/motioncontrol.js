72765fdcd4e9942268e4bc594d1a7e0a3942097f
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-12-29 23:03:04 +0100

    Release 0.2.0

--- dist/motioncontrol.js
+++ dist/motioncontrol.js
@@ -14,26 +14,37 @@
   }
 }(this, function () {
 
-  /*! motioncontrol.js - v0.1.3
-   *  Release on: 2014-12-28
+  /*! motioncontrol.js - v0.2.0
+   *  Release on: 2014-12-29
    *  Copyright (c) 2014 Stéphane Bachelier
    *  Licensed MIT */
   'use strict';
 
   /* From Modernizr */
-  var transitionEvent = function () {
-    var t;
-    var el = document.createElement('fakeelement');
-    var transitions = {
-      transition:'transitionend',
-      OTransition:'oTransitionEnd',
-      MozTransition:'transitionend',
-      WebkitTransition:'webkitTransitionEnd'
-    };
+  // animation events
+  var animationEvents = {
+    animation: 'animationend',
+    OAnimation: 'oAnimationEnd',
+    MozAnimation: 'animationend',
+    WebkitAnimation: 'webkitAnimationEnd',
+    MSAnimation: 'MSAnimationEnd'
+  };
 
-    for (t in transitions) {
-      if (undefined !== el.style[t]) {
-        return transitions[t];
+  // transition events
+  var transitionEvents = {
+    transition: 'transitionend',
+    OTransition: 'oTransitionEnd',
+    MozTransition: 'transitionend',
+    WebkitTransition: 'webkitTransitionEnd'
+  };
+
+  var findEvent = function (eventMap) {
+    var property;
+    var el = document.createElement('fakeelement');
+
+    for (property in eventMap) {
+      if (undefined !== el.style[property]) {
+        return eventMap[property];
       }
     }
   };
@@ -43,16 +54,30 @@
   };
 
   var motioncontrol = (function () {
+    var addEventsListener = function (el, callback, events) {
+      for (var event in events) {
+        el.addEventListener(event, callback, false);
+      }
+    };
+
+    var removeEventsListener = function (el, callback, events) {
+      for (var event in events) {
+        el.removeEventListener(event, callback, false);
+      }
+    };
+
     return function (el, options) {
       var resolver = {};
       var opts = options || {};
       var trigger = opts.trigger && isFunction (opts.trigger) ? opts.trigger :  null;
       var timeout = opts.timeout || 1000;
 
-      var event = transitionEvent();
+      var events = [findEvent(transitionEvents), findEvent(animationEvents)];
 
       var motionComplete = function () {
-        el.removeEventListener(event, motionComplete, false);
+        if (events) {
+          removeEventsListener(el, motionComplete, events);
+        }
 
         if (!resolver) {
           // we can safely return as the resolver is only set to null
@@ -80,17 +105,16 @@
       });
 
       // add motion end control detector function
-      if (event) {
+      if (events) {
         // add listeners
-        el.addEventListener(event, motionComplete, false);
+        addEventsListener(el, motionComplete, events);
       }
-      else {
-        // add a timeout in case where the transitionend event would not be fired
-        // which basically make this library :
-        // * fake non CSS3 browsers or
-        // * fake a transition end if the property is removed
-        setTimeout(motionComplete, timeout);
-      }
+
+      // add a timeout in case where the transitionend event would not be fired
+      // which basically make this library :
+      // * fake non CSS3 browsers or
+      // * fake a transition end if the property is removed
+      setTimeout(motionComplete, timeout);
 
       if (trigger) {
         trigger();
